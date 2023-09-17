b4860198762ab0a7e845dc28806b4dd2bc7045f2
fabiomatos999 <fabio.matos@upr.edu>
2023-09-17 17:28:36 -0400

    Change database schema to use sqlite types

--- M.O.S.I.S Host Software/App/app.py
+++ M.O.S.I.S Host Software/App/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.sqlite import TEXT, REAL, INTEGER
+
+
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -12,23 +15,23 @@ db = SQLAlchemy(app)
 
 class media_entry(db.Model):
     __tablename__ = "media_entry"
-    entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    shot_type = db.Column(db.String(20), nullable=False)
+    entry_id = db.Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
+    shot_type = db.Column(TEXT, nullable=False)
 
     def __repr__(self):
         return '<Task %r>' % self.entry_id
 
 class media_metadata(db.Model):
     __tablename__ = "media_metadata"
-    metadata_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    entry_id = db.Column(db.Integer, db.ForeignKey(media_entry.entry_id))
-    left_camera_media = db.Column(db.String(200), nullable=False)
-    right_camera_media = db.Column(db.String(200), nullable=False)
-    time = db.Column(db.String(40), nullable=False)
-    temperature = db.Column(db.Float, nullable=False)
-    pressure = db.Column(db.Float, nullable=False)
-    ph = db.Column(db.Float, nullable=False)
-    dissolved_oxygen = db.Column(db.Float, nullable=False)
+    metadata_id = db.Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
+    entry_id = db.Column(INTEGER, db.ForeignKey(media_entry.entry_id))
+    left_camera_media = db.Column(TEXT, nullable=False)
+    right_camera_media = db.Column(TEXT, nullable=False)
+    time = db.Column(TEXT, nullable=False)
+    temperature = db.Column(REAL, nullable=False)
+    pressure = db.Column(REAL, nullable=False)
+    ph = db.Column(REAL, nullable=False)
+    dissolved_oxygen = db.Column(REAL, nullable=False)
     
     def __repr__(self):
         return '<Task %r>' % self.metadata_id
