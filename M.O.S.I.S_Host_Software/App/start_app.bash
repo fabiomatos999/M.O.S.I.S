c29b1c538dde356bfe788bce303b98ceeb10a12a
fabiomatos999 <fabio.matos@upr.edu>
2023-10-01 10:25:43 -0400

    Created startup script

---
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if ! command -v python3 &> /dev/null; then
+    echo "Python is not installed"
+    exit
+fi
+if [ ! -d $(pwd)"/venv" ]; then
+    python3 -m venv $(pwd)"/venv"
+    source $(pwd)"/venv/bin/activate"
+    pip install -r $(pwd)"/requirements.txt"
+fi
+source $(pwd)"/venv/bin/activate"
+./app.py
