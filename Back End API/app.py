5167e56e36c6c05f8c495cd2b9e7ce5c9132fa98
eduardomiranda3 <eduardo.miranda3@upr.edu>
2023-10-05 11:12:34 -0400

    Start database create media_entry and media_metadata table

--- Back End API/app.py
+++ Back End API/app.py
@@ -1,10 +1,38 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-import sys
+import sqlite3
 
+# Connect to or create an SQLite database file. If it doesn't exist, a new database will be created.
+conn = sqlite3.connect('my_database.db')
 
-app = QApplication(sys.argv)
+# Create a cursor object to interact with the database.
+cursor = conn.cursor()
 
-window = QWidget()
-window.show()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS media_entry (
+        entryId INTEGER PRIMARY KEY,
+        shotType TEXT NOT NULL,
+        time TEXT NOT NULL,
+        illuminationType TEXT NOT NULL,
+        iso INTEGER NOT NULL,
+        apertureSize REAL NOT NULL,
+        shutterSpeed REAL NOT NULL,
+        whiteBalance INTEGER NOT NULL
+    )
+''')
 
-sys.exit(app.exec())
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS media_metadata (
+        metadataId INTEGER PRIMARY KEY,
+        entryId INTEGER,
+        leftCameraMedia TEXT NOT NULL,
+        rightCameraMedia TEXT NOT NULL,
+        time TEXT NOT NULL,
+        temperature REAL NOT NULL,
+        pressure REAL NOT NULL,
+        ph REAL NOT NULL,
+        dissolvedOxygen REAL NOT NULL,
+        FOREIGN KEY (entryId) REFERENCES media_entry(entryId)
+    )
+''')
+# Commit the changes and close the database connection when done.
+conn.commit()
+conn.close()
