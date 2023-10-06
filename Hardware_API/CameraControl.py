fba129093e8459b51928f960f46f88a4095f3325
fabiomatos999 <fabio.matos@upr.edu>
2023-10-06 13:58:05 -0400

    Fix documentation styling.

--- Hardware_API/CameraControl.py
+++ Hardware_API/CameraControl.py
@@ -130,8 +130,9 @@ class CameraControl():
                 print("max Gain value: ", self.maxGainValue)
 
     def setUpCamera(self, num_cameras=1):
+        """Initialize both cameras for M.O.S.I.S microscope."""
         main_hCameras = []
-        # First: Determine how many cameras are connected and available for connecting
+        "# First: Determine how many cameras are connected and are available"
         ret = PxLApi.getNumberCameras()
         "print(ret)"
         if PxLApi.apiSuccess(ret[0]):
@@ -149,10 +150,11 @@ class CameraControl():
                         hCamera = ret[1]
                         main_hCameras.append(hCamera)
                         if i == 0:
-                            params = [
-                                0, 1
-                            ]  #params dictate that horizontal flip will be off (0) while vertical flip is on (1)
-                            #flips image of the left camera
+                            params = [0, 1]
+                            # params dictate that horizontal flip will be off
+                            # (0) while vertical flip is on (1)
+
+                            # flips image of the left camera
                             ret2 = PxLApi.setFeature(
                                 hCamera, PxLApi.FeatureId.FLIP,
                                 PxLApi.FeatureFlags.MANUAL, params)
@@ -175,9 +177,12 @@ class CameraControl():
                                title="",
                                leftOffset=-10):
         """
-        Function to be executed by a thread in charge of showing the camera preview
-        :param topHwnd: integer number that corresponds to the parent window the stream will be showed in
-        :return: an assertion that the stream was successfully stopped, once the user exits the preview
+        Thread  function in charge of showing the camera preview.
+
+        :param topHwnd: integer number that corresponds to the parent window
+        the stream will be showed in
+        :return: an assertion that the stream was successfully stopped,
+        once the user exits the preview
         """
         width = 400
         height = 400
@@ -189,51 +194,46 @@ class CameraControl():
 
         ret = PxLApi.setPreviewState(hCamera, PxLApi.PreviewState.START)
 
-        #         while (PxLApi.PreviewState.START == self.previewState and PxLApi.apiSuccess(ret[0])):
-        #             pass
-        #             if(PxLApi.PreviewState.STOP == self.previewState):
-        #                 print("stop")
-        #                 # User has exited -- Stop the preview
-        #                 ret = PxLApi.setPreviewState(hCamera, PxLApi.PreviewState.STOP)
         assert PxLApi.apiSuccess(ret[0]), "%i" % ret[0]
 
     def create_new_preview_thread(self, hCamera, topHwnd):
-        """
-            Creates a new preview thread for each preview run
-        """
-        # Creates a thread with preview control / meessage pump
+        """Create a new preview thread for each preview run."""
         return threading.Thread(target=self.control_preview_thread,
                                 args=(hCamera, topHwnd),
                                 daemon=True)
 
     def start_preview(self, stream_width, stream_height, hCamera, topHwnd):
         """
-           Start the preview (with message pump).
-           Preview gets stopped when the top level window is closed.
+        Start the preview (with message pump).
+
+        Preview gets stopped when the top level window is closed.
         """
         self.setUpStreamSize(stream_width, stream_height)
 
-        # Declare control preview thread that can control preview and poll the message pump on Windows
+        # Declare control preview thread that can control preview and poll the
+        # message pump on Windows
         self.previewState = PxLApi.PreviewState.START
         previewThread = self.create_new_preview_thread(hCamera, topHwnd)
         previewThread.start()
 
     def cleanUpCameras(self, hCamera):
+        """Set Preview State for both cameras to Stop."""
         for i in range(len(hCamera)):
             PxLApi.setStreamState(hCamera[i], PxLApi.StreamState.STOP)
-            ret = PxLApi.setPreviewState(hCamera[i], PxLApi.PreviewState.STOP)
+            # ret =\
+            #     PxLApi.setPreviewState(hCamera[i], PxLApi.PreviewState.STOP)
             PxLApi.uninitialize(hCamera[i])
 
     def setFocus(self, hCamera, newfocusValue=2, mode="auto"):
         """
-        Prompts the camera to perform autofocus
+        Prompt the camera to perform autofocus.
+
         :param hCamera: the camera to perform autofocus
         :return:
         """
-
-        #params = []
-        if mode is "auto":
-            #params.insert(0, self.minFocusValue)
+        # params = []
+        if mode == "auto":
+            # params.insert(0, self.minFocusValue)
             #             params.insert(1, self.maxFocusValue)
 
             for i in range(len(hCamera)):
@@ -250,7 +250,7 @@ class CameraControl():
                 print("focus value not in acceptable range")
                 return
 
-            #self.params.insert(0, newfocusValue)
+            # self.params.insert(0, newfocusValue)
             self.customFocus[0] = newfocusValue
 
             for i in range(len(hCamera)):
@@ -265,18 +265,19 @@ class CameraControl():
 
     def setExposure(self, hCamera, newExposureValue, mode="auto"):
         """
-        Prompts the camera to perform autofocus
-        :param hCamera: the camera to perform autofocus
+        Set exposure for a camera.
+
+        :param hCamera: the camera to change exposure
         :return:
         """
-
-        if not self.minExposureValue <= newExposureValue <= self.maxExposureValue:
+        if not\
+           self.minExposureValue <= newExposureValue <= self.maxExposureValue:
             print("focus value not in acceptable range", newExposureValue)
             return
 
         params = []
 
-        if mode is "auto":
+        if mode == "auto":
             params = []
             params.insert(0, self.minExposureValue)
             params.insert(1, self.minExposureValue)
@@ -303,18 +304,19 @@ class CameraControl():
 
     def setSaturation(self, hCamera, newSaturationValue, mode="auto"):
         """
-        Prompts the camera to perform autofocus
-        :param hCamera: the camera to perform autofocus
+        Set exposure value for a camera.
+
+        :param hCamera: the camera to change saturation
         :return:
         """
-
-        if not self.minSaturationValue <= newSaturationValue <= self.maxSaturationValue:
+        if not self.minSaturationValue <=\
+           newSaturationValue <= self.maxSaturationValue:
             print("focus value not in acceptable range", newSaturationValue)
             return
 
         params = []
 
-        if mode is "auto":
+        if mode == "auto":
             params = []
             params.insert(0, self.minSaturationValue)
             params.insert(1, self.maxSaturationValue)
@@ -344,18 +346,18 @@ class CameraControl():
 
     def setGain(self, hCamera, newGainValue, mode="auto"):
         """
-        Prompts the camera to perform autofocus
-        :param hCamera: the camera to perform autofocus
+        Set the gain value for a camera.
+
+        :param hCamera: the camera to set gain
         :return:
         """
-
         if not self.minGainValue <= newGainValue <= self.maxGainValue:
             print("focus value not in acceptable range", newGainValue)
             return
 
         params = []
 
-        if mode is "auto":
+        if mode == "auto":
             params = []
             params.insert(0, self.minGainValue)
             params.insert(1, self.maxGainValue)
