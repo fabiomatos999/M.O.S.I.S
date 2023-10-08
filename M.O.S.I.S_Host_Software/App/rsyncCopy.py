f1a806d78c081a435d1fed9b86abb62616ec4522
fabiomatos999 <fabio.matos@upr.edu>
2023-10-08 16:01:37 -0400

    Create rsync recursive copy module

---
+++ M.O.S.I.S_Host_Software/App/rsyncCopy.py
@@ -0,0 +1,17 @@
+"""rsync subproccess recursive copy module."""
+import subprocess
+
+
+def rsync_recursive_copy(source: str, destination: str):
+    """
+    Call rsync and recursively copy files from source to destination.
+
+    Will raise an exception if rsync has invalid source
+    or destination arguments or if rsync is not installed
+    in system path.
+    """
+    try:
+        subprocess.check_call(["rsync", "-rv", source, destination],
+                              stdout=subprocess.DEVNULL)
+    except Exception as err:
+        raise err
