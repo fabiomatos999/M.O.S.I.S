cc04dfa703ae3b51ee455fa9d6d3081cbf3af9e4
eduardomiranda3 <eduardo.miranda3@upr.edu>
2023-10-10 09:53:39 -0400

    Fix formatting and add dissolved oxygen

--- Back End API/media.py
+++ Back End API/media.py
@@ -1,23 +1,26 @@
-class media_entry():
-  # Defines the media entry class for the Media Entry table
-    def __init__(self, entry_id, shot_type, time, illumination_type, iso, aperture_size, shutter_speed, white_balance):
-        self.entry_id = entry_id
-        self.shot_type = shot_type
+class media_Entry():
+    # Defines the media entry class for the Media Entry table
+    def __init__(self, entry_Id, shot_Type, time, illumination_Type, iso,
+                 aperture_Size, shutter_Speed, white_Balance):
+        self.entry_Id = entry_Id
+        self.shot_Type = shot_Type
         self.time = time
-        self.illumination_type = illumination_type
+        self.illumination_Type = illumination_Type
         self.iso = iso
-        self.aperture_size = aperture_size
-        self.shutter_speed = shutter_speed
-        self.white_balance = white_balance
+        self.aperture_Size = aperture_Size
+        self.shutter_Speed = shutter_Speed
+        self.white_Balance = white_Balance
 
-class media_metadata():
-    #Defines the media metadata class for the Media Metadata table
-    def __init__(self, meta_dataid, entry_id, leftcameramedia, rightcameramedia, time, temperature, pressure, ph):
-        self.meta_dataid = meta_dataid
-        self.entry_id = entry_id
-        self.leftcameramedia = leftcameramedia
-        self.rightcameramedia = rightcameramedia
+
+class media_Metadata():
+    # Defines the media metadata class for the Media Metadata table
+    def __init__(self, entry_Id, left_Camera_Media, right_Camera_Media, time, temperature,
+                 pressure, ph, dissolved_Oxygen):
+        self.entry_Id = entry_Id
+        self.left_Camera_Media = left_Camera_Media
+        self.right_Camera_Media = right_Camera_Media
         self.time = time
         self.temperature = temperature
         self.pressure = pressure
         self.ph = ph
+        self.dissolved_Oxygen = dissolved_Oxygen
