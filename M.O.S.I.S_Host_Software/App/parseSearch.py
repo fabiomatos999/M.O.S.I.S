7e57d1a2f8c042263ba1af9ed35fe8a054d292bb
fabiomatos999 <fabio.matos@upr.edu>
2023-10-11 11:35:57 -0400

    Add csv and single parsing to search query

--- M.O.S.I.S_Host_Software/App/parseSearch.py
+++ M.O.S.I.S_Host_Software/App/parseSearch.py
@@ -5,25 +5,45 @@ from routes import db
 
 dualEndedRangeRegex = r'^\d+-\d+$'
 leftEndedRangeragex = r'^\d+-$'
-rightEndedRangeRegex = r'-\d+$'
+rightEndedRangeRegex = r'^-\d+$'
+commaSeparatedValueRegex = r'^(\d+,)+\d+$'
+singleValueRagex = r'^\d+$'
+
 
 def parseIdRange(searchQuery: str) -> [int]:
-    left = int()
-    right = int()
     dbMin = getFirstMediaEntry(db)
     dbMax = getLastMediaEntry(db)
-    if bool(re.search(dualEndedRangeRegex, searchQuery)):
-        left = int(searchQuery.split('-')[0])
-        right = int(searchQuery.split('-')[-1])
-    elif bool(re.search(leftEndedRangeragex, searchQuery)):
-        left = int(searchQuery.split('-')[0])
-        right = dbMax
-    elif bool(re.search(rightEndedRangeRegex, searchQuery)):
-        left = dbMin
-        right = int(searchQuery.split('-')[-1])
-    else:
-        left = dbMin
-        right = dbMax
+    entries = set()
+    left = int()
+    right = int()
+    for query in searchQuery.split(" "):
+        if bool(re.search(dualEndedRangeRegex, query)):
+            left = int(query.split('-')[0])
+            right = int(query.split('-')[-1])
+            for i in returnRange(left, right, dbMin, dbMax):
+                entries.add(int(i))
+        elif bool(re.search(leftEndedRangeragex, query)):
+            left = int(query.split('-')[0])
+            right = dbMax
+            for i in returnRange(left, right, dbMin, dbMax):
+                entries.add(int(i))
+        elif bool(re.search(rightEndedRangeRegex, query)):
+            left = dbMin
+            right = int(query.split('-')[-1])
+            for i in returnRange(left, right, dbMin, dbMax):
+                entries.add(int(i))
+        elif bool(re.search(commaSeparatedValueRegex, query)):
+            values = query.split(',')
+            for i in values:
+                entries.add(int(i))
+        elif bool(re.search(singleValueRagex, query)):
+            entries.add(int(query))
+    entries = list(filter(lambda x: x >= dbMin and x <= dbMax, entries))
+    entries.sort()
+    return entries
+
+
+def returnRange(left: int, right: int, dbMin: int, dbMax: int) -> [int]:
     if left > right:
         temp = left
         left = right
@@ -32,4 +52,4 @@ def parseIdRange(searchQuery: str) -> [int]:
         left = dbMin
     if right > dbMax:
         right = dbMax
-    return list(range(left, right+1, 1))
+    return list(range(left, right + 1, 1))
