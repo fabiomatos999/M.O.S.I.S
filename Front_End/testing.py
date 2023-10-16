80fcc577e97ce5cbd2adcf40be912b3f887b1e02
eduardomiranda3 <eduardo.miranda3@upr.edu>
2023-10-16 18:26:39 -0400

    Add ApertureSize Menu with buttons changing label

--- Front_End/testing.py
+++ Front_End/testing.py
@@ -4,12 +4,13 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 # from DissolvedOxygenCalibrationMenu import Ui_Form
 from WhiteBalanceCalibrationMenu import Ui_Form
 from IsoConfigurationMenu import Ui_ISOConfigurationMenu
-
+from ApertureSizeConfigurationMenu import Ui_ApertureSizeConfigurationMenu
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
        # self.ui = Ui_Form()
         self.ui = Ui_ISOConfigurationMenu()
+        self.ui = Ui_ApertureSizeConfigurationMenu()
         self.ui.setupUi(self)
 
 if __name__ == "__main__":
