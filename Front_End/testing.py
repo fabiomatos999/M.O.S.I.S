76a8f3a0a8e1e8c08826961e8f214145c09eba0a
eduardomiranda3 <eduardo.miranda3@upr.edu>
2023-10-17 07:54:30 -0400

    Add ShutterSpeed Menu with functioning buttons

--- Front_End/testing.py
+++ Front_End/testing.py
@@ -1,18 +1,18 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-# from phSensorCalibrationMenu import Ui_Form
-# from DissolvedOxygenCalibrationMenu import Ui_Form
-from WhiteBalanceCalibrationMenu import Ui_Form
+from phSensorCalibrationMenu import Ui_Form
+from DissolvedOxygenCalibrationMenu import Ui_Form
+#from WhiteBalanceCalibrationMenu import Ui_Form
 from IsoConfigurationMenu import Ui_ISOConfigurationMenu
 from ApertureSizeConfigurationMenu import Ui_ApertureSizeConfigurationMenu
 from ShutterSpeedConfigurationMenu import Ui_ShutterSpeedConfigurationMenu
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-       # self.ui = Ui_Form()
+        self.ui = Ui_Form()
         self.ui = Ui_ISOConfigurationMenu()
-        self.ui = Ui_ApertureSizeConfigurationMenu()
-        #self.ui = Ui_ShutterSpeedConfigurationMenu()
+     #   self.ui = Ui_ApertureSizeConfigurationMenu()
+        self.ui = Ui_ShutterSpeedConfigurationMenu()
         self.ui.setupUi(self)
 
 if __name__ == "__main__":
