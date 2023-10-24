6e2fb4ee5c4317cef12bf95a23f83fa5bc9e3f40
fabiomatos999 <fabio.matos@upr.edu>
2023-10-24 09:34:58 -0400

    Add json metadata generation functionality

--- Back_End_API/FolderStructureGenerator.py
+++ Back_End_API/FolderStructureGenerator.py
@@ -8,10 +8,19 @@ class FolderStructureGenerator:
         self.root_path = root_path
         self.databaseQuery = databaseQuery.DatabaseQuery()
 
-    def create_folder_structure(self, folders):
+    def create_folder_structure(self, folders: str):
         for folder in folders:
             folder_path = os.path.join(self.root_path, folder)
             os.makedirs(folder_path, exist_ok=True)
+            id = int(folder.split("-")[0])
+            metadata = self.databaseQuery.getAllMediaMetadaByEntryId(id)
+            metadata = list(map(lambda x: x.__dict__, metadata))
+            jsonContents = json.dumps(metadata, indent=4)
+            entryFolder = os.path.join(self.root_path, folder)
+            file = open(os.path.join(entryFolder, "metadata.json"), "w")
+            file.write(jsonContents)
+            file.close()
+
 
 
 if __name__ == "__main__":
