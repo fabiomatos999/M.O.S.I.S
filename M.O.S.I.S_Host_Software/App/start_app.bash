4e7740fe16365bf9aefd8294e68625f4310792c1
fabiomatos999 <fabio.matos@upr.edu>
2023-10-28 17:09:53 -0400

    Add rsync to install script

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -4,7 +4,7 @@ if ! command -v python3 &> /dev/null; then
     exit
 fi
 if [ ! -d $(pwd)"/venv" ]; then
-    sudo apt install python3-venv -y
+    sudo apt install python3-venv rsync -y
     python3 -m venv $(pwd)"/venv"
     source $(pwd)"/venv/bin/activate"
     pip install -r $(pwd)"/requirements.txt"
