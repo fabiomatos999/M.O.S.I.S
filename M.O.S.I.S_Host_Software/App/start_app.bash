5199d35549e819b920f48d792ae121d9ec157516
fabiomatos999 <fabio.matos@upr.edu>
2023-10-28 17:06:06 -0400

    Add install venv to start script

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -4,6 +4,7 @@ if ! command -v python3 &> /dev/null; then
     exit
 fi
 if [ ! -d $(pwd)"/venv" ]; then
+    sudo apt install python3-venv -y
     python3 -m venv $(pwd)"/venv"
     source $(pwd)"/venv/bin/activate"
     pip install -r $(pwd)"/requirements.txt"
