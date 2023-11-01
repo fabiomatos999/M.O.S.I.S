b256112f4a1ec1e6d70d67690053c74228a936ef
fabiomatos999 <fabio.matos@upr.edu>
2023-11-01 13:37:30 -0400

    Migrate all SQLAlchemy queries to python3 sqlite.

--- M.O.S.I.S_Host_Software/App/db.py
+++ M.O.S.I.S_Host_Software/App/db.py
@@ -1,6 +1,9 @@
 """Database interaction module."""
 import sqlite3
 from enums import illuminationType, shotType
+from representations import MediaEntryInternalRepresentation, \
+    MediaMetadataInternalRepresentation
+import re
 
 
 class DatabaseQuery:
@@ -44,14 +47,169 @@ class DatabaseQuery:
         """Close db connection when object is destroyed."""
         self.conn.close()
 
-    def insertMediaEntry(self, entryId: int, shottype: shotType,
-                         illuminationtype: illuminationType, saturation: int,
-                         gain: float, shutterSpeed: str, whiteBalance: int):
+    def insertMediaEntry(self, entryId: int, shottype: shotType, time: str,
+                         illuminationtype: illuminationType, gain: float,
+                         saturation: int, shutterSpeed: str,
+                         whiteBalance: int):
         self.cursor.execute(
             """INSERT INTO MediaEntry
             (entryId, shotType, time,
-            illuminationType, gain, saturation, whiteBalance)
-            VALUES (?, ?,?,?,?,?,?)""",
-            (entryId, shottype, illuminationtype, gain, saturation,
-             shutterSpeed, whiteBalance))
+            illuminationType, gain, saturation, shutterSpeed, whiteBalance)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (entryId, shottype.name, time, illuminationtype.name, gain,
+             saturation, shutterSpeed, whiteBalance))
         self.conn.commit()
+
+    def getMediaEntry(self, entryId: int) -> MediaEntryInternalRepresentation:
+        ret = self.cursor.execute("SELECT * FROM MediaEntry WHERE entryId = ?",
+                                  (entryId, )).fetchone()
+        return DatabaseQuery.mediaEntryTableToInternalRepresentation(ret)
+
+    def getAllMediaEntryIDs(self):
+        ret = self.cursor.execute("SELECT entryId FROM MediaEntry").fetchall()
+        return list(map(lambda x: x[0], ret))
+
+    def getAllMediaEntry(self) -> [MediaEntryInternalRepresentation]:
+        ret = self.cursor.execute("SELECT * FROM MediaEntry").fetchall()
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaEntryTableToInternalRepresentation(x), ret))
+
+    def insertMediaMetadata(self, metadataId: int, entryId: int,
+                            leftCameraMedia: str, rightCameraMedia, time: str,
+                            temperature: float, pressure: float, ph: float,
+                            dissolvedOxygen: float):
+        self.cursor.execute(
+            """INSERT INTO MediaMetadata
+            (metadataId, entryId, leftCameraMedia,
+            rightCameraMedia, time, temperature,
+            pressure, ph, dissolvedOxygen)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            (metadataId, entryId, leftCameraMedia, rightCameraMedia, time,
+             temperature, pressure, ph, dissolvedOxygen))
+        self.conn.commit()
+
+    def getAllMediaMetadataId(
+            self, entryId: int) -> [MediaMetadataInternalRepresentation]:
+        ret = self.cursor.execute(
+            "SELECT * FROM MediaMetadata WHERE entryId = ?",
+            (entryId, )).fetchall()
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaMetadataTableToInternalRepresentation(x), ret))
+
+    def getAllMediaMetadata(self) -> [MediaMetadataInternalRepresentation]:
+        ret = self.cursor.execute("SELECT * FROM MediaMetadata").fetchall()
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaMetadataTableToInternalRepresentation(x), ret))
+
+    def getFirstMediaEntry(self) -> int:
+        ret = self.cursor.execute(
+            """SELECT entryId FROM MediaEntry ORDER BY entryId ASC LIMIT 1"""
+        ).fetchone()
+        return int(ret[0])
+
+    def getLastMediaEntry(self) -> int:
+        ret = self.cursor.execute(
+            """SELECT entryId FROM MediaEntry ORDER BY entryId DESC LIMIT 1"""
+        ).fetchone()
+        return int(ret[0])
+
+    def getMediaEntriesByShotType(
+            self, shottype: str) -> [MediaEntryInternalRepresentation]:
+        ret = self.cursor.execute(
+            "SELECT * FROM MediaEntry WHERE shotType = ?", (shottype, ))
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaEntryTableToInternalRepresentation(x), ret))
+
+    def getMediaEntriesByTime(self,
+                              time: str) -> [MediaEntryInternalRepresentation]:
+        ret = self.cursor.execute(
+            "SELECT * FROM MediaEntry WHERE time LIKE %?%", time)
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaEntryTableToInternalRepresentation(x), ret))
+
+    def getMediaEntriesByIlluminationType(
+            self, illuminationtype: str) -> [MediaEntryInternalRepresentation]:
+        ret = self.cursor.execute(
+            "SELECT * FROM MediaEntry WHERE illuminationType = ?",
+            (illuminationtype, ))
+        return list(
+            map(
+                lambda x: DatabaseQuery.
+                mediaEntryTableToInternalRepresentation(x), ret))
+
+    @staticmethod
+    def mediaEntryTableToInternalRepresentation(dbQuery: (
+    )) -> MediaEntryInternalRepresentation:
+        return MediaEntryInternalRepresentation(dbQuery[0], dbQuery[1],
+                                                dbQuery[2], dbQuery[3],
+                                                dbQuery[4], dbQuery[5],
+                                                dbQuery[6], dbQuery[7])
+
+    def mediaMetadataTableToInternalRepresentation(dbQuery: (
+    )) -> MediaMetadataInternalRepresentation:
+        return MediaMetadataInternalRepresentation(dbQuery[0], dbQuery[1],
+                                                   dbQuery[2], dbQuery[3],
+                                                   dbQuery[4], dbQuery[5],
+                                                   dbQuery[6], dbQuery[7],
+                                                   dbQuery[8])
+
+    def parseIdRange(self, searchQuery: str) -> [int]:
+        dualEndedRangeRegex = r'^\d+-\d+$'
+        leftEndedRangeragex = r'^\d+-$'
+        rightEndedRangeRegex = r'^-\d+$'
+        commaSeparatedValueRegex = r'^(\d+,)+\d+$'
+        singleValueRagex = r'^\d+$'
+        dbMin = self.getFirstMediaEntry()
+        dbMax = self.getLastMediaEntry()
+        entries = set()
+        left = int()
+        right = int()
+
+        def returnRange(left: int, right: int, dbMin: int,
+                        dbMax: int) -> [int]:
+            if left > right:
+                temp = left
+                left = right
+                right = temp
+                if left < dbMin:
+                    left = dbMin
+                    if right > dbMax:
+                        right = dbMax
+                        return list(range(left, right + 1, 1))
+
+        for query in searchQuery.split(" "):
+            if bool(re.search(dualEndedRangeRegex, query)):
+                left = int(query.split('-')[0])
+                right = int(query.split('-')[-1])
+                for i in returnRange(left, right, dbMin, dbMax):
+                    entries.add(int(i))
+            elif bool(re.search(leftEndedRangeragex, query)):
+                left = int(query.split('-')[0])
+                right = dbMax
+                for i in returnRange(left, right, dbMin, dbMax):
+                    entries.add(int(i))
+            elif bool(re.search(rightEndedRangeRegex, query)):
+                left = dbMin
+                right = int(query.split('-')[-1])
+                for i in returnRange(left, right, dbMin, dbMax):
+                    entries.add(int(i))
+            elif bool(re.search(commaSeparatedValueRegex, query)):
+                values = query.split(',')
+                for i in values:
+                    entries.add(int(i))
+            elif bool(re.search(singleValueRagex, query)):
+                entries.add(int(query))
+                entries = list(
+                    filter(lambda x: x >= dbMin and x <= dbMax, entries))
+                entries.sort()
+                return entries
