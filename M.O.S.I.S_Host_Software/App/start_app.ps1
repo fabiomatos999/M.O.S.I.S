ece49c8c6ab98d8f2991fe106c7309561d437381
fabiomatos999 <fabio.matos@upr.edu>
2023-11-07 14:15:03 -0400

    Add dependency check for start_app.ps1

--- M.O.S.I.S_Host_Software/App/start_app.ps1
+++ M.O.S.I.S_Host_Software/App/start_app.ps1
@@ -1,2 +1,18 @@
+if (!(Test-Path -Path 'C:\Program Files\wkhtmltopdf\*')) {
+    Write-Error "wkhtmltopdf is not installed: Please install it from here: https://wkhtmltopdf.org/downloads.html"
+    exit
+}
+if (!(Test-Path -Path 'C:\Program Files\gs\*')) {
+    Write-Error "GhostScript is not installed: Please install it from here: https://www.ghostscript.com/releases/gsdnld.html"
+}
+if (!(Get-Command python)) {
+    Write-Error "Python is not installed: Please install it from here: https://www.python.org/downloads/"
+}
+if (!(Test-Path -Path '.\venv')) {
+    mkdir .\venv
+    python -m venv .\venv
+    .\venv\Scripts\Activate.ps1
+    pip install -r .\requirements-win.txt
+}
 .\venv\Scripts\Activate.ps1
 python -m app $args
