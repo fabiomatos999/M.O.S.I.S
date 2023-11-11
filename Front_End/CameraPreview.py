7b697f73fc29f5a10ba3239e28be983de797a565
fabiomatos999 <fabio.matos@upr.edu>
2023-11-11 09:48:00 -0400

    Document database query and camera preview modules.

--- Front_End/CameraPreview.py
+++ Front_End/CameraPreview.py
@@ -1,21 +1,25 @@
+"""Image manipulation module for preview screen."""
 import CameraPictureControl
-import CameraControl
 import cv2
 
 
-def getPreviewImage(hCamera: [int], path: str) -> str:
+def getPreviewImage(cameraHandles: [int], folderPath: str) -> str:
+    """Generate preview screen images and resize images to 400x400.
+
+    :param cameraHandles list of camera handles from the
+    PxLApi initialize function
+    :param folderPath folder where the preview screen images will
+    be outputted
+    """
     cpc = CameraPictureControl.CameraPictureControl()
-    cc = CameraControl.CameraControl()
-    print(hCamera)
-    #cc.setRegionOfInterest(hCamera, 1096, 772, 400, 400)
-    lCamName = path + str(hCamera[0]) + ".jpeg"
-    rCamName = path + str(hCamera[1]) + ".jpeg"
-    cpc.get_snapshot(hCamera[0], lCamName)
-    cpc.get_snapshot(hCamera[1], rCamName)
+    lCamName = folderPath + str(cameraHandles[0]) + ".jpeg"
+    rCamName = folderPath + str(cameraHandles[1]) + ".jpeg"
+    cpc.get_snapshot(cameraHandles[0], lCamName)
+    cpc.get_snapshot(cameraHandles[1], rCamName)
     lImg = cv2.imread(lCamName)
     lImg = cv2.resize(lImg, (400, 400))
     cv2.imwrite(lCamName, lImg)
     rImg = cv2.imread(rCamName)
     rImg = cv2.resize(rImg, (400, 400))
     cv2.imwrite(rCamName, rImg)
-    return path
+    return folderPath
