56daaf46fcf8c8e13812d8954cd82dfbf662744e
fabiomatos999 <fabio.matos@upr.edu>
2023-11-11 14:42:12 -0400

    Add focus stack requirements check and bug fixes.

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-if ! command wkhtmltopdf; then
+if ! command -v wkhtmltopdf &> /dev/null; then
     sudo apt install wkhtmltopdf -y
 fi
-if ! command ghostscript; then
+if ! command -v ghostscript &> /dev/null; then
     sudo apt install ghostscript -y
 fi
-if ! command python3; then
+if ! command -v python3 &> /dev/null; then
     sudo apt install python3 python3-venv -y
+fi
+if ! command -v focus-stack &> /dev/null; then
+    >&2 echo "FocusStack if not installed. Please download it from here: https://github.com/PetteriAimonen/focus-stack/releases/"
+    exit 1
 fi
 if [ ! -d $(pwd)"/venv" ]; then
     python3 -m venv $(pwd)"/venv"
