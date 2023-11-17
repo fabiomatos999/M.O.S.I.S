b2172f74053b5f3b013dd7c258cc5b6c1dd5d993
fabiomatos999 <fabio.matos@upr.edu>
2023-11-17 17:58:14 -0400

    Add SysCheck and fix integer division bug.

--- DummySensorHub/DummySensorHub.ino
+++ DummySensorHub/DummySensorHub.ino
@@ -1,6 +1,5 @@
 void setup() {
   Serial.begin(115200);
-  Serial.println("[1111]");
 }
 
 void loop() {
@@ -10,20 +9,23 @@ void loop() {
   String input = Serial.readString();
   if (input == "read") {
     char phBuffer[7];
-    float randpH = rand()%15 + (rand()%100)/100;
+    float randpH = rand()%14 + (float)(rand()%100)/100;
     dtostrf(randpH, 0, 3, phBuffer);
     String ph = phBuffer;
     char tempBuffer[8];
-    float randTemp = rand() % 35 - 2 + (rand()%100)/100;
+    float randTemp = rand() % 34 - 2 + (float)(rand()%100)/100;
     dtostrf(randTemp, 0, 3, tempBuffer);
     String temp = tempBuffer;
     char dissolvedOxygenBuffer[8];
-    float randDissolvedOxygen = rand() % 401 + (rand()%100)/100;
+    float randDissolvedOxygen = rand() % 401 + (float)(rand()%100)/100;
     dtostrf(randDissolvedOxygen, 0, 3, dissolvedOxygenBuffer);
     String dissolvedOxygen = dissolvedOxygenBuffer;
-    String pressure = String(rand() % 10000000);
+    String pressure = String(rand() % 9026);
     String msg =
         ph + "&" + temp + "&" + dissolvedOxygen + "&" + pressure + "\0";
     Serial.println(msg);
+  }
+  else if(input == "SysCheck") {
+    Serial.println("1111");
   }
 }
