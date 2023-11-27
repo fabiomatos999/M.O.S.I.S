05fd36b7cb027b0233f6d573fca8ae087ed6b102
eduardomiranda3 <46756465+eduardomiranda3@users.noreply.github.com>
2023-11-27 11:41:26 -0400

    Add 3 decimal points to dummy sensor hub

--- DummySensorHub/DummySensorHub.ino
+++ DummySensorHub/DummySensorHub.ino
@@ -9,15 +9,15 @@ void loop() {
   String input = Serial.readString();
   if (input == "read") {
     char phBuffer[7];
-    float randpH = rand()%14 + (float)(rand()%100)/100;
+    float randpH = rand()%14 + (float)(rand()%1000)/1000;
     dtostrf(randpH, 0, 3, phBuffer);
     String ph = phBuffer;
     char tempBuffer[8];
-    float randTemp = rand() % 34 - 2 + (float)(rand()%100)/100;
+    float randTemp = rand() % 34 - 2 + (float)(rand()%1000)/1000;
     dtostrf(randTemp, 0, 3, tempBuffer);
     String temp = tempBuffer;
     char dissolvedOxygenBuffer[8];
-    float randDissolvedOxygen = rand() % 400 + (float)(rand()%100)/100;
+    float randDissolvedOxygen = rand() % 400 + (float)(rand()%1000)/1000;
     dtostrf(randDissolvedOxygen, 0, 3, dissolvedOxygenBuffer);
     String dissolvedOxygen = dissolvedOxygenBuffer;
     String pressure = String(rand() % 9026);
