c744d8978ed5e56e1f3188cff428372189a4c8ac
fabiomatos999 <fabio.matos@upr.edu>
2023-12-01 07:52:42 -0400

    Add install of focus-stack to linux install script.

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -9,13 +9,19 @@ if ! command -v python3 &> /dev/null; then
     sudo apt install python3 python3-venv -y
 fi
 if ! command -v focus-stack &> /dev/null; then
-    >&2 echo "FocusStack if not installed. Please download it from here: https://github.com/PetteriAimonen/focus-stack/releases/"
-    exit 1
+    sudo apt install git libopencv-dev build-essential -y
+    git clone https://github.com/PetteriAimonen/focus-stack.git
+    cd focus-stack
+    make
+    sudo make install
+    cd ..
+    rm -rf focus-stack
 fi
 if [ ! -d $(pwd)"/venv" ]; then
     python3 -m venv $(pwd)"/venv"
     source $(pwd)"/venv/bin/activate"
     pip install -r $(pwd)"/requirements.txt"
 fi
+mkdir static/Media
 source $(pwd)"/venv/bin/activate"
 ./app.py $@
