a5004ff2ca5c586c8b5e856067b92ec3d539ab70
fabiomatos999 <fabio.matos@upr.edu>
2023-12-01 08:30:15 -0400

    Fix bug where venv would never install on debian.

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -6,7 +6,10 @@ if ! command -v ghostscript &> /dev/null; then
     sudo apt install ghostscript -y
 fi
 if ! command -v python3 &> /dev/null; then
-    sudo apt install python3 python3-venv -y
+    sudo apt install python3 -y
+fi
+if [ $(sudo apt -qq list python3-venv 2> /dev/null | awk '/installed/') == "" ]; then
+    sudo apt install python3-venv -y
 fi
 if ! command -v focus-stack &> /dev/null; then
     sudo apt install git libopencv-dev build-essential -y
@@ -22,6 +25,6 @@ if [ ! -d $(pwd)"/venv" ]; then
     source $(pwd)"/venv/bin/activate"
     pip install -r $(pwd)"/requirements.txt"
 fi
-mkdir static/Media
+mkdir static/Media 2> /dev/null
 source $(pwd)"/venv/bin/activate"
 ./app.py $@
