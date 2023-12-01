5f87873f2b35d63762be100be8adbfc39df340f4
fabiomatos999 <fabio.matos@upr.edu>
2023-12-01 09:04:10 -0400

    Remove unnecessary sudo

--- M.O.S.I.S_Host_Software/App/start_app.bash
+++ M.O.S.I.S_Host_Software/App/start_app.bash
@@ -8,7 +8,7 @@ fi
 if ! command -v python3 &> /dev/null; then
     sudo apt install python3 -y
 fi
-if [ "$(sudo apt -qq list python3-venv 2> /dev/null | awk '/installed/')" == "" ]; then
+if [ "$(apt -qq list python3-venv 2> /dev/null | awk '/installed/')" == "" ]; then
     sudo apt install python3-venv -y
 fi
 if ! command -v focus-stack &> /dev/null; then
