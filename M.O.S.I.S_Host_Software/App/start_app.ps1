a873b0255d0e16ab0e856487caf1dca059b0b9f5
fabiomatos999 <fabio.matos@upr.edu>
2023-12-02 10:16:14 -0400

    Add dependency install and path reload to start_app.ps1

--- M.O.S.I.S_Host_Software/App/start_app.ps1
+++ M.O.S.I.S_Host_Software/App/start_app.ps1
@@ -1,3 +1,4 @@
+Unblock-File .\*.ps1
 if (!(Test-Path -Path 'C:\Program Files\wkhtmltopdf\*')) {
     Start-Process .\redist\wkhtmltox-0.12.6-1.msvc2015-win64.exe -Wait
 }
@@ -8,10 +9,14 @@ if (!(Test-Path C:\Users\*\AppData\Local\Programs\Python\Python312\python.exe)) 
     Start-Process .\redist\python-3.12.0-amd64.exe -Wait
 }
 if (!(Test-Path -Path '.\venv')) {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
     mkdir .\venv
     python -m venv .\venv
     .\venv\Scripts\Activate.ps1
     pip install -r .\requirements-win.txt
+}
+if (!(Test-Path -Path '.\static\Media')) {
+    mkdir .\static\Media
 }
 .\venv\Scripts\Activate.ps1
 python -m app $args
